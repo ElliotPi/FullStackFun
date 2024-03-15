4dc3e11572ba74e65b292cb572284b2dda7f87d6
ElliotPi <103081209+ElliotPi@users.noreply.github.com>
2024-03-14 22:44:52 -0600

    A million changes

---
+++ backend/APIFun/Controllers/MarriottFoodController.cs
@@ -0,0 +1,26 @@
+﻿using APIFun.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIFun.Controllers
+{
+    [Route("[controller]")]  // You could define what you want, the original: [Route("api/[controller]")]
+    [ApiController]
+    public class MarriottFoodController : ControllerBase
+    {
+        private IFoodRepository _foodRepository;
+        public MarriottFoodController(IFoodRepository temp) 
+        { 
+            _foodRepository = temp;
+        }
+
+        [HttpGet]
+        public IEnumerable<MarriottFood> Get()
+        {
+            var foodData = _foodRepository.Foods.ToArray();
+
+            return foodData;
+        }
+
+    }
+}
---
+++ backend/APIFun/Data/EFFoodRepository.cs
@@ -0,0 +1,12 @@
+﻿
+namespace APIFun.Data
+{
+    public class EFFoodRepository : IFoodRepository
+    {
+        private FoodContext _foodcontext;
+        public EFFoodRepository(FoodContext temp) {
+            _foodcontext = temp;
+        }
+        public IEnumerable<MarriottFood> Foods => _foodcontext.Foods;
+    }
+}
